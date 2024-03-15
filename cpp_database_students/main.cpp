c44b83a59a8c89283fb4cab2ed33737d68dbb2d3
iPlugin <ipluginua@gmail.com>
2024-03-15 11:12:05 +0200

    Automated commit

--- cpp_database_students/main.cpp
+++ cpp_database_students/main.cpp
@@ -11,8 +11,6 @@ int main(){
 
     PGconnector connector(dbname, user, password, host, port);
 
-
-
     // int id = 9;
     // string name = "Andriy";
     // string surname = "Dutka";
@@ -23,33 +21,12 @@ int main(){
     // connector.rm(id);
     connector.show();
 
-
-
-    // CppTable table(5);
-    // table.add_row("ID Name Surname Group Ticket");
-    // table.add_row("1 Andriy Dutka 12PZ 123123");
-    // table.add_row("2 Tania Ruzhman 43TR 321321");
-    // table.add_row("3 Misha Trach 22KI 123456");
-    // table.print_table();
     return 0;
 }
 
 /*
-
-Пофіксити клас class_table він має отримувати
-result result = txn.exec(query); і нормально
-його обробляти
-
-PGconnector::show()
-main -> PGconnector - class_table -> main
-
-Створити config file для 
-- dbname
-- user
-- password
-- host
-- port
-
+! Проблема з виводом headers
+Створити config
 
 */
 // ! написати функцію fill_word
