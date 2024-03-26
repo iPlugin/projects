231eb4a132cf04b061349cb16b306e189492830f
iPlugin <ipluginua@gmail.com>
2024-03-26 15:53:44 +0200

    Automated commit

---
+++ py_caesars_cipher/caesars_cipher.py
@@ -0,0 +1,45 @@
+from string import ascii_uppercase
+
+LETTER = ascii_uppercase
+
+while True:
+    print("Do you want to [1]encrypt or [2]decrypt?")
+    res = input(">>> ").lower()
+    if res == "1":
+        action = "encrypt"
+        break
+    elif res == "2":
+        action = "decrypt"
+        break
+    
+    print("Please enter the number 1 or 2")
+    
+while True:
+    key1 = len(LETTER) - 1
+    print(f"Please enter the key (0 to {key1}) to use")
+    res = input(">>> ").upper()
+    if not res.isdecimal():
+        continue
+    
+    if 0 <= int(res) < len(LETTER):
+        key2 = int(res)
+        break
+    
+print(f"Enter the message to {action}")
+msg = input(">>> ").upper()
+
+translated = ""
+
+for char in msg:
+    if char in LETTER:
+        num = LETTER.find(char)
+        if action == "encrypt":
+            num = (num + key2) % len(LETTER)
+        elif action == "decrypt":
+            num = (num - key2) % len(LETTER)
+    
+        translated += LETTER[num]
+    else:
+        translated += char
+        
+print("\n" + translated)
---
+++ py_vigener_cipher/vigener_cipher.py
@@ -0,0 +1,73 @@
+from string import ascii_uppercase
+
+LETTERS = ascii_uppercase
+
+def main():
+    while True:
+        print("Do you want to [1]encrypt or [2]decrypt?")
+        res = input(">>> ").lower()
+        if res == "1":
+            action = "encrypt"
+            break
+        elif res == "2":
+            action = "decrypt"
+            break
+        
+        print("Please enter the number 1 or 2")
+    
+    while True:
+        print("Please specify the key to use")
+        print("It can be a word or any combination of letters:")
+        res = input(">>> ").upper()
+        if res.isalpha():
+            key = res
+            break
+        
+    print(f"Enter the message to {action}")
+    msg = input(">>> ")
+    if action == "encrypt":
+        translated = encryptMessage(msg, key)
+    elif action == "decrypt":
+        translated = decryptMessage(msg, key)
+    
+    print(f"{action.title()}ed message:")
+    print(translated)
+    
+    
+def encryptMessage(message, key):
+    return translateMessage(message, key, 'encrypt')
+
+def decryptMessage(message, key):
+    return translateMessage(message, key, 'decrypt')
+
+def translateMessage(message, key, mode):
+    translated = []
+    keyIndex = 0
+    key = key.upper()
+
+    for char in message:
+        num = LETTERS.find(char.upper())
+        if num != -1:
+            if mode == "encrypt":
+                num += LETTERS.find(key[keyIndex])
+            elif mode == "decrypt":
+                num -= LETTERS.find(key[keyIndex])
+                
+            num %= len(LETTERS)
+            
+            if char.isupper():
+                translated.append(LETTERS[num])
+            elif char.islower():
+                translated.append(LETTERS[num].lower())
+        else:
+            translated.append(char)
+        
+        keyIndex += 1
+        if keyIndex == len(key):
+            keyIndex = 0
+
+    return "".join(translated)
+
+    
+if __name__ == "__main__":
+    main()
